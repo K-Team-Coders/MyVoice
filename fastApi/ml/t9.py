b231c00807a5302833ddce1ac3180a99d1a7dbe3
Anton0009 <114334201+Anton0009@users.noreply.github.com>
2023-09-09 19:06:19 +0300

    Update t9.py

--- fastApi/ml/t9.py
+++ fastApi/ml/t9.py
@@ -4,9 +4,12 @@ def t9(text):
     spell = SpellChecker(language='ru')
     text_split = text.split()
     cor = ""
-    for word in text_split:
-        if spell.correction(word) == word:
-            cor += word+' '
-        else:
-            cor += spell.correction(word)+' '
+    try:
+        for word in text_split:
+            if spell.correction(word) == word:
+                cor += word+' '
+            else:
+                cor += spell.correction(word)+' '
+    except TypeError:
+        cor = text
     return {"t9_corretion" : cor}
