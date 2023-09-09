b86785efb6208c938dad54571fe85863c75c6b1d
qTemiO <71713499+qTemiO@users.noreply.github.com>
2023-09-09 11:20:35 +0300

    udp clustering

--- fastApi/main.py
+++ fastApi/main.py
@@ -2,15 +2,23 @@ import os
 import time
 
 import psycopg2
+import numpy as np
 from loguru import logger
-from fastapi import FastAPI, Response, File, UploadFile
+from typing import Any, List
+from fastapi import FastAPI, Response, File, UploadFile, Query
 from fastapi.responses import JSONResponse  
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from transformers import AutoTokenizer, AutoModel
+from sklearn.cluster import KMeans,MeanShift,DBSCAN,MiniBatchKMeans,AgglomerativeClustering
+from sklearn.metrics.pairwise import cosine_similarity
+
+
 
 # ml features
 from ml.tone import tone
 from ml.t9 import t9
+from ml.services import get_text, get_bert_embeddings
 
 # Initial code
 # Database connection setup
@@ -43,6 +51,11 @@ try:
 
 except Exception as e:
     logger.error(f'Docker DB connect failed \n {e}!')
+
+# Clusterisation models preset
+model_name = "bert-base-multilingual-cased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModel.from_pretrained(model_name)
 
 app = FastAPI()
 
@@ -107,3 +120,43 @@ def filesProcessing(file: UploadFile):
     data = file.file.read()
     logger.debug(data)
     return Response(status_code=201)
+
+@app.post("/clusters")
+def get_s_text(text: List[str] = Query(None)):
+
+    res=[]
+    # bac_req={}
+    for i in range(0,len(text)):
+        res.append(get_text(text[i]))
+    logger.debug(res)
+    text_embeddings = get_bert_embeddings(res)
+
+    # Вычисление матрицы попарных косинусных сходств
+    cosine_matrix = cosine_similarity(text_embeddings)
+    #вычисление оптима кластеров
+    wcss = {}
+    limit = int((len(text)//2)**0.9)
+
+    for k in range(2,limit+1):
+        model = KMeans(n_clusters=k)
+        model.fit(1-cosine_matrix)
+        wcss[k] = model.inertia_
+    # Рассчитываем дифференциал значений
+    differences = {k: abs(v - wcss[k - 1]) if k > 2 else 0 for k, v in wcss.items()}
+
+    # Находим ключ с максимальным дифференциалом
+    max_key = max(differences, key=differences.get)
+
+    # Кластеризация с использованием HDBSCAN
+    clusterer = KMeans(max_key)
+    cluster_labels = clusterer.fit_predict(1 - cosine_matrix)
+    logger.debug(int((len(text)//2)**0.8))
+    logger.debug(cluster_labels)
+
+    # Группировка текстов по кластерам
+    clusters = {}
+    for cluster_id in np.unique(cluster_labels):
+        cluster_docs = np.array(res)[cluster_labels == cluster_id]
+        clusters[f"Кластер {cluster_id}"] = cluster_docs.tolist()
+
+    return clusters
