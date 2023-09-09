9a2aa92eaf4c60815364927669984bcc755ff834
Shisha <shishkarev16@gmail.com>
2023-09-09 17:22:10 +0300

    +addAlert

--- frontend/src/views/TablePage.vue
+++ frontend/src/views/TablePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <body >
+  <body>
     <Header2></Header2>
     <div class="bg-idealblack">
       <p class="text-whitesmoke text-3xl text-center font-semibold pt-8">
@@ -14,6 +14,18 @@
         </div>
       </div>
     </div>
+    <div v-if="created">
+      <transition-group
+        enter-active-class="transition ease-out duration-100 "
+        enter-from-class="transition opacity-0 scale-95"
+        enter-to-class="transform opacity-100 scale-100"
+        leave-active-class="transition ease-in duration-100"
+        leave-from-class="transform opacity-100 scale-100"
+        leave-to-class="tranfrom opacity-0 scale-95"
+      >
+        <Alert></Alert>
+      </transition-group>
+    </div>
     <Footer></Footer>
   </body>
 </template>
@@ -23,6 +35,7 @@ import Header2 from "@/components/Header2.vue";
 import Forms from "@/components/Forms.vue";
 import TagCloud from "@/components/TagCloud.vue";
 import Footer from "@/components/Footer.vue";
+import Alert from "@/components/Alert.vue";
 import { mapActions, mapGetters } from "vuex";
 export default {
   components: {
@@ -30,6 +43,7 @@ export default {
     Forms,
     TagCloud,
     Footer,
+    Alert,
   },
   computed: {
     ...mapGetters(["allDrones"]),
