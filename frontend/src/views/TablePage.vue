3edf4f9c6ff868c69c65cfd7f22ab37e156d74ea
Shisha <shishkarev16@gmail.com>
2023-09-10 10:26:29 +0300

    Update TablePage.vue

--- frontend/src/views/TablePage.vue
+++ frontend/src/views/TablePage.vue
@@ -3,16 +3,23 @@
     <Header2></Header2>
     <LoadingPage v-if="isLoading" />
     <div v-else class="bg-idealblack">
-      <p class="text-whitesmoke 2xl:text-3xl text-lg text-center font-semibold pt-8">
+      <p
+        class="text-whitesmoke 2xl:text-3xl text-lg text-center font-semibold pt-8"
+      >
         {{ result.headQuestion }}
       </p>
       <div class="py-10">
-        <div class="flex justify-center">
+        <div class="flex flex-col gap-2 items-center justify-center">
           <Forms></Forms>
-          <button @click="DownloadFile()" type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none h-24 focus:ring-blue-300 font-semibold rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-3">
-            Экспорт результатов
-          </button>
+          <div class="">
+            <button
+              @click="DownloadFile()"
+              type="submit"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none h-12 focus:ring-blue-300 font-semibold rounded-lg text-sm w-full sm:w-auto px-5 py-1 text-center"
+            >
+              Экспорт результатов
+            </button>
+          </div>
         </div>
         <div>
           <TableMetrics :metrics="result.metrics" />
@@ -53,32 +60,37 @@ export default {
   },
   methods: {
     DownloadFile() {
-      axios.post(`http://${process.env.VUE_APP_USER_IP_WITH_PORT}/export/${this.$route.params.id}/`)
-        .then(response => {
-          const link = document.createElement('a')
+      axios
+        .post(
+          `http://${process.env.VUE_APP_USER_IP_WITH_PORT}/export/${this.$route.params.id}/`
+        )
+        .then((response) => {
+          const link = document.createElement("a");
           link.href = window.URL.createObjectURL(new Blob([response.data]));
           link.setAttribute(
-            'download',
+            "download",
             `labeled_${this.$route.params.id}.json`
-          )
+          );
           document.body.appendChild(link);
-          link.click()
-        })
-    }
+          link.click();
+        });
+    },
   },
-created(){
-  this.isLoading = true
+  created() {
+    this.isLoading = true;
 
-  axios.post(`http://${process.env.VUE_APP_USER_IP_WITH_PORT}/tabledetailview/${this.$route.params.id}/`)
-    .then((res) => {
-
-      this.result = res.data.result
-    }
-    )
-    .finally(() => {
-      this.isLoading = false;
-    })
-}}
+    axios
+      .post(
+        `http://${process.env.VUE_APP_USER_IP_WITH_PORT}/tabledetailview/${this.$route.params.id}/`
+      )
+      .then((res) => {
+        this.result = res.data.result;
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  },
+};
 </script>
 
 <style></style>
